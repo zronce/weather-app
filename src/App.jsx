99ac47cb22dbb7854349f02726ce21e8449990d8
Kyle <183276654+kylebatac@users.noreply.github.com>
2024-11-03 16:59:04 +0800

    added some changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import Weather from './components/weather'
-
+import './App.css'
 
 const App = () => {
   return (
+    
     <div className="app">
+      <div className='title-container'>
+      <h1 className='title'>WEATHER PUBLIC</h1>
+      <p className='subtitle'>Get the latest weather information for your city.</p> 
+      </div>
+      
       <Weather />
       
     </div>
